7e012b812063f85c344943725822e98dc19cfbaa
abdielbytes <wilsonabdiel86@gmail.com>
2024-12-20 20:08:56 +0100

    wip

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -3,8 +3,20 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class TaskController extends Controller
 {
-    //
+    public function create(Request $request)
+    {
+        $validate = Validator::make($request->all(), [
+            'task' => 'required|string|max:255',
+            'deadline'=>'required|date'
+        ]);
+
+        if ($validate->fails()) {
+            return response()->json($validate->errors(), 400);
+        }
+        return response()->json(['message' => 'Task created successfully'], 201);
+    }
 }
